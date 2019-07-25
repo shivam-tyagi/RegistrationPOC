4530a4e34f294942e68025d9bb459edf23100ab0
anoop kumar <anoop.kumar#magicedtech.com>
2019-07-26 05:31:55 +1300

    shareComponent

--- src/client/src/components/templates/TemplateShareComponent.js
+++ src/client/src/components/templates/TemplateShareComponent.js
@@ -27,42 +27,99 @@ class TemplatesShareComponent extends Component {
 
   render(){
     return(
-      <div className='templates-share-page'>
+      <div className='templates-edit-page templates-share-page'>
         <Navbar {...this.props}></Navbar>
         <div className="upperHead">
           <Link to="/edit"><i className="headIcon"></i> Edit Card</Link>
         </div>
         <div className='share-page-container'>
           <div className='template-share-container-left'>
-            <div className='template-to-share'>
+            <div className='template-to-share clearfix'>
               <div className='share-template-img'>
 
               </div>
               <div className='template-data'>
-                <div className='share-template-title'>
-                  Template title
-                </div>
+                <h2 className='share-template-title'>
+                   Christmas Celibration
+                </h2>
                 <div className='template-share-link'>
-
+                      <span>Share event link with guests</span>
+                      <ul className="social_list"><li><a href="javascript:void(0)" class="Socialicon googleIcon"></a></li><li><a href="javascript:void(0)" class="Socialicon linkedinIcon"></a></li><li><a href="javascript:void(0)" class="Socialicon facebookIcon"></a></li></ul>
+                      <a href="https://xd.adobe.com/view/456" target="_blank" className="adobeLink">https://xd.adobe.com/view/456</a>
                 </div>
               </div>
             </div>
             <div className='guest-list-container'>
-              <div> Add Guests by email or phone</div>
-              <div className='import-contact-btn'>Import Contact</div>
-              <div className='upload-contact-btn'>Upload Contact</div>
-              <div className='deleteall-btn' onClick={this.deleteAll}>Delete All</div>
-              <div className='add-contact-textarea'>
-                <input id="eventTitle" value={this.state.guestList} onChange={this.onChange} type="text" placeholder='Enter or pPaste emails or phone numbers seperated by semi-colons.' />
-              </div>
-              <div className='add-guests-btn'>Add Guest</div>
+              <h3> Add Guests by email or phone</h3>
+               <div className="contactWrapper">
+                  <a href="javascript:void(0)" className='import-contact-btn'>Import Contact</a>
+                  <a href="javascript:void(0)" className='upload-contact-btn'>Upload Contact</a>
+               </div>
+               <div className="inputDeleteWrapper">
+                  <a className='deleteall-btn' onClick={this.deleteAll}chref="javascript:void(0)">Delete All</a>
+                  <div className='add-contact-textarea'>
+                    <input id="eventTitle" value={this.state.guestList} onChange={this.onChange} type="text" placeholder='Enter or pPaste emails or phone numbers seperated by semi-colons.' />
+                  </div>
+               </div>
+              {/* <div className='add-guests-btn'>Add Guest</div> */}
             </div>
           </div>
           <div className='template-share-container-right'>
-            <div>Guest List</div>
-            <div>Guest list is currently blank</div>
+            <h2>Guest List</h2>
+            {/* <div>Guest list is currently blank</div>
             <div className='save-sent-later-btn'>Save &amp; Sent Later</div>
-            <div className='finish-sent-btn'>Finish Send</div>
+            <div className='finish-sent-btn'>Finish Send</div> */}
+
+              <div class="tableWrapper">
+                   <table>
+                         <thead>
+                             <tr>
+                                 <th>Name</th>
+                                 <th>Email/Mobile</th>
+                                 <th>Action</th>
+                             </tr>
+                         </thead>
+                          <tbody>
+                             <tr>
+                                 <td>Shivam Tyagi</td>
+                                 <td>Shivam@gmail.com</td>
+                                 <td><a href="javascript:void(0)">Edit</a><a href="javascript:void(0)">Delete</a></td>
+                             </tr>
+                             <tr>
+                                 <td>Shivam Tyagi</td>
+                                 <td>Shivam@gmail.com</td>
+                                 <td><a href="javascript:void(0)">Edit</a><a href="javascript:void(0)">Delete</a></td>
+                             </tr>
+                             <tr>
+                                 <td></td>
+                                 <td></td>
+                                 <td></td>
+                               </tr>
+                               <tr>
+                                 <td></td>
+                                 <td></td>
+                                 <td></td>
+                               </tr>
+                               <tr>
+                                 <td></td>
+                                 <td></td>
+                                 <td></td>
+                               </tr>
+                               <tr>
+                                 <td></td>
+                                 <td></td>
+                                 <td></td>
+                               </tr>
+                               <tr>
+                                 <td></td>
+                                 <td></td>
+                                 <td></td>
+                               </tr>
+                          </tbody>
+
+                      
+                     </table>
+              </div>
           </div>
         </div>
       </div>
